ef107ff7adc946f9e79193704251b7eff1ba0925
Loide Mara Verdes <mara.verdes@gmail.com>
2019-08-21 17:58:25 -0400

    Provision linux instance
    - Upload key pair to access instance
    - Provision instance with file to install nginx

--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "sa-east-1"
+  default = "us-east-1"
 }
 variable "AMIS" {
   type = "map"
@@ -11,4 +11,14 @@ variable "AMIS" {
     eu-west-1 = "ami-0d729a60"
     sa-east-1 = "ami-041bfc9131f33e2a2"
   }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
 }
