2a1f5eeb7a6317838dda9adea753d2124876ff5a
Loide Mara Verdes <mara.verdes@gmail.com>
2019-09-04 22:34:16 -0400

    Add VPC

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-w2e3"
+    bucket = "terraform-state-q1w2"
     key = "terraform/demo4"
   }
 }
--- instance.tf
+++ instance.tf
@@ -1,12 +1,15 @@
-resource "aws_key_pair" "mykey" {
-  key_name = "mykey"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
-}
-
 resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.mykey.key_name}"
+
+  # the VPC subnet
+  subnet_id = "${aws_subnet.main-public-1.id}"
+
+  # the security group
+  vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+
+  # the public ssh key
+  key_name = "${aws_key_pair.mykeypair.key_name}"
 
   provisioner "local-exec" {
     command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
@@ -22,12 +25,6 @@ resource "aws_instance" "example" {
       "chmod +x /tmp/script.sh",
       "sudo /tmp/script.sh"
     ]
-  }
-
-  connection {
-    host = "${self.public_ip}"
-    user = "${var.INSTANCE_USERNAME}"
-    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
   }
 }
 
